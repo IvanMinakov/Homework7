b054432f76a5c33bd30479540d13f1e5c5b01e74
Ivan Minakov <i.minakov786@gmail.com>
2022-12-12 14:08:45 -0500

    task1 done

---
+++ task1/Program.cs
@@ -0,0 +1,24 @@
+﻿void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+
+            array[i, j] = new Random().Next(-10, 11);
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j],3}\t");
+        System.Console.WriteLine();
+    }
+}
+System.Console.WriteLine("Введие полкичество столбцов");
+int rows = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введие полкичество строк");
+int cols = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[rows, cols];
+FillArray(array);
+PrintArray(array);
