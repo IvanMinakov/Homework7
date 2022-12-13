ed40cc69ed07d4436e8c907fb023814c3f777941
Ivan Minakov <i.minakov786@gmail.com>
2022-12-13 12:54:17 -0500

    task2 done

--- task2/Program.cs
+++ task2/Program.cs
@@ -3,7 +3,7 @@
     for (int i = 0; i < array.GetLength(0); i++)
         for (int j = 0; j < array.GetLength(1); j++)
 
-            array[i, j] = new Random().Next(-10, 11);
+            array[i, j] = new Random().Next(1, 51);
 }
 
 void PrintArray(int[,] array)
@@ -16,6 +16,25 @@ void PrintArray(int[,] array)
     }
 }
 
+void FindNumber(int[,] array)
+{
+    int posx = 0;
+    int posy = 0;
+    bool check = false;
+    System.Console.WriteLine("Введие искомое число");
+    int number = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            if (array[i, j] == number)
+            {
+                posy = i;
+                posx = j;
+                System.Console.WriteLine($"Положение в массиве элемента {number} находится в cтроке {posy + 1} и столбце {posx + 1} ");
+                check = true;
+            }
+    if (check == false) System.Console.WriteLine("Такого числа в матрице нет");
+}
 System.Console.WriteLine("Введие количество строк");
 int rows = Convert.ToInt32(Console.ReadLine());
 System.Console.WriteLine("Введие количество столбцов");
@@ -23,3 +42,5 @@ int cols = Convert.ToInt32(Console.ReadLine());
 int[,] array = new int[rows, cols];
 FillArray(array);
 PrintArray(array);
+System.Console.WriteLine();
+FindNumber(array);
